95fedf85bfc68199d79a09957f6ad1841cda2fa0
Nashoba <93211280+Nashoba1er@users.noreply.github.com>
2024-12-20 15:14:44 +0100

    Add files via upload
    Premier Commit pour le projet Serrato

---
+++ serrato/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.7"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm")
+}
+
+group = "com.emse.spring"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ serrato/src/test/java/com/emse/spring/serrato/SerratoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emse.spring.serrato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerratoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
